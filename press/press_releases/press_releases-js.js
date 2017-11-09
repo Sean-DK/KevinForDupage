a2b78e4a269c5f59d87670152ef5a446d351de39
Sean Gaffney <smgaffney@neo.rr.com>
2017-11-08 22:14:54 -0500

    Added additional pages

---
+++ press/press_releases/press_releases-js.js
@@ -0,0 +1,11 @@
+function fade() {
+    document.body.classList.remove('fade-out');
+}
+
+function displayDropdown() {
+    document.getElementsByClassName("dropdown-content")[0].style.display = "inline-block";
+}
+
+function hideDropdown() {
+    document.getElementsByClassName("dropdown-content")[0].style.display = "none";
+}
